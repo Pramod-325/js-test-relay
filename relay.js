a71b8bb60b89a361d7874075fb94325e61a24017
Pramod-325 <147941686+Pramod-325@users.noreply.github.com>
2026-01-15 17:36:33 +0530

    Create relay.js

---
+++ relay.js
@@ -0,0 +1,47 @@
+// @ts-check
+import { createLibp2p } from 'libp2p'
+import { autoNAT } from '@libp2p/autonat'
+import { identify } from '@libp2p/identify'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
+import { webSockets } from '@libp2p/websockets'
+import { tcp } from '@libp2p/tcp'
+import { webRTC, webRTCDirect } from '@libp2p/webrtc'
+import { circuitRelayServer, circuitRelayTransport } from '@libp2p/circuit-relay-v2'
+import { PUBSUB_PEER_DISCOVERY } from './constants.js'
+
+async function main() {
+  // enable('*')
+  const libp2p = await createLibp2p({
+    addresses: {
+      listen: [
+        '/ip4/0.0.0.0/tcp/9001/ws',
+        '/ip4/0.0.0.0/tcp/9002',
+        '/ip4/0.0.0.0/udp/9090/webrtc-direct',
+      ],
+    },
+    transports: [
+      tcp(),
+      webSockets(),
+      webRTC(),
+      webRTCDirect(),
+      circuitRelayTransport()
+    ],
+    connectionEncrypters: [noise()],
+    streamMuxers: [yamux()],
+    services: {
+      identify: identify(),
+      autoNat: autoNAT(),
+      relay: circuitRelayServer(),
+      pubsub: gossipsub(),
+    },
+  })
+
+  libp2p.services.pubsub.subscribe(PUBSUB_PEER_DISCOVERY)
+
+  console.log('PeerID: ', libp2p.peerId.toString())
+  console.log('Multiaddrs: ', libp2p.getMultiaddrs())
+}
+
+main()
